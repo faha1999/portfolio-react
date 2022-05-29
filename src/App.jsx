4b96993cf756d3a6183e4fa2b06527c6f817300c
faha1999 <kafaha1999@gmail.com>
2022-05-29 21:09:22 +0600

    applied react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { lightTheme, DarkTheme } from './components/Themes';
+import { Route, Routes } from 'react-router-dom';
+import { About } from './components/About';
+import { Main } from './components/Main';
+import { Blog } from './components/Blog';
+import { Work } from './components/Work';
+import { Skill } from './components/Skill';
+
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <>
+      <div theme={DarkTheme}>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/work" element={<Work />} />
+          <Route path="/skill" element={<Skill />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Work.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Work = () => {
+  return <div>Work</div>;
+};
