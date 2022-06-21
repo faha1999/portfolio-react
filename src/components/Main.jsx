170eb9f276ac63f3036febd878477512ca7e9fb9
faha1999 <kafahad1999@gmial.com>
2022-06-22 02:15:17 +0600

    added navigation menu

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { LogoComponent } from './partials/LogoComponent';
 import { PowerButton } from './partials/PowerButton';
 import { SocialIcons } from './partials/SocialIcons';
@@ -10,6 +11,32 @@ export const Main = () => {
         <PowerButton />
         <LogoComponent />
         <SocialIcons />
+
+        <NavLink
+          className="contact"
+          target="_blank"
+          to={{ pathname: 'mailto:kafahad1999@gmail.com' }}
+        >
+          <h3>Say hi...</h3>
+        </NavLink>
+
+        <NavLink className="blog" to="./blog">
+          <h3>Blog</h3>
+        </NavLink>
+
+        <NavLink className="work" to="./work">
+          <h3>Work</h3>
+        </NavLink>
+
+        <div className="bottomBar">
+          <NavLink className="about" to="./about">
+            <h3>About</h3>
+          </NavLink>
+
+          <NavLink className="skills" to="./skill">
+            <h3>My skills</h3>
+          </NavLink>
+        </div>
       </div>
     </div>
   );
