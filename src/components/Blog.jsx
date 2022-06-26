c1abe83fbfb0ed232f6b1edaeb5345136f32a1d6
Kawsar Ahmed Fahad <kafaha1999@gmail.com>
2022-06-27 01:04:40 +0600

    blog setup done

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
+import { LogoComponent } from './partials/LogoComponent';
+import { PowerButton } from './partials/PowerButton';
+import { SocialIcons } from './partials/SocialIcons';
+
+const Container = styled.div`
+  background-color: ${(props) => `rgba(${props.theme.bodyRgba},0.8)`};
+`;
 
 export const Blog = () => {
-  return <div></div>;
+  return (
+    <div className="mainContainer">
+      <Container className="container">
+        <LogoComponent />
+        <PowerButton />
+        <SocialIcons />
+
+        <div className="center">
+          <div className="grid">
+            <h1>blogs</h1>
+            <h1>blogs</h1>
+            <h1>blogs</h1>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 };
