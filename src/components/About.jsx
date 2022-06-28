829d90be4a70a2da9cfcdcaf951adba2198f77c3
Kawsar Ahmed Fahad <kafaha1999@gmail.com>
2022-06-29 03:13:18 +0600

    added about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,47 @@
 import React from 'react';
+import { DarkTheme, lightTheme } from './Themes';
+import styled, { ThemeProvider } from 'styled-components';
+import astronaut from '../assets/Images/spaceman.png';
+
+import { LogoComponent } from './partials/LogoComponent';
+import { PowerButton } from './partials/PowerButton';
+import { SocialIcons } from './partials/SocialIcons';
+
+const Box = styled.div`
+  background-color: ${(props) => props.theme.body};
+`;
+
+const AboutMe = styled.div`
+  border: 2px solid ${(props) => props.theme.text};
+  color: ${(props) => props.theme.text};
+`;
 
 export const About = () => {
-  return <div>About</div>;
+  return (
+    <ThemeProvider theme={DarkTheme}>
+      <LogoComponent theme={DarkTheme} />
+      <SocialIcons theme={DarkTheme} />
+      <PowerButton />
+
+      <Box className="About">
+        <div className="spaceman">
+          <img src={astronaut} alt="spaceman" />
+        </div>
+
+        <AboutMe className="AboutMe">
+          People find me to be an upbeat, self-motivated team player with
+          excellent communication & organizational skills. I have not only
+          headed any project but also ensured its success by demonstrating
+          leadership abilities.
+          <br /> <br />
+          Besides that I always feel very joyous and fortunate to meet and greet
+          people belonging to different backgrounds and cultures. As such
+          meetings are always important because they prove to be beneficial in
+          future also it makes things easy to cope whether one works or studies
+          in his own country or outside the country. I believe “Never stop
+          dreaming because my success is hiding inside my dream.’’
+        </AboutMe>
+      </Box>
+    </ThemeProvider>
+  );
 };
---
+++ src/components/partials/AboutParticle.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const AboutParticle = () => {
+  return <div>AboutParticle</div>;
+};
