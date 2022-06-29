b5c3a89d012c0afb30c58405882723be9adf50d7
Kawsar Ahmed Fahad <kafaha1999@gmail.com>
2022-06-30 02:56:13 +0600

    Applied work horizontal scroll

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import { DarkTheme } from './Themes';
 
 import { LogoComponent } from './partials/LogoComponent';
 import { PowerButton } from './partials/PowerButton';
 import { SocialIcons } from './partials/SocialIcons';
-
+import { YinYang } from './AllSvgs';
 import { WorkDetails } from '../data/WorkData';
 import { WorkCard } from './partials/WorkCard';
 
@@ -13,7 +13,30 @@ const Box = styled.div`
   background-color: ${(props) => props.theme.body};
 `;
 
+// const Main = styled.ul`
+//   position: fixed;
+// `;
+
 export const Work = () => {
+  const ref = useRef(null);
+  const yinyang = useRef(null);
+
+  useEffect(() => {
+    let element = ref.current;
+
+    const rotate = () => {
+      element.style.transform = `translateX(${-window.pageYOffset}px)`;
+
+      return (yinyang.current.style.transform =
+        'rotate(' + -window.pageYOffset + 'deg)');
+    };
+
+    window.addEventListener('scroll', rotate);
+    return () => {
+      window.removeEventListener('scroll', rotate);
+    };
+  }, []);
+
   return (
     <ThemeProvider theme={DarkTheme}>
       <Box className="Work">
@@ -21,11 +44,15 @@ export const Work = () => {
         <SocialIcons theme="dark" />
         <PowerButton />
 
-        <div className="WorkDetails">
+        <div className="WorkDetails" ref={ref}>
           {WorkDetails.map((d) => (
             <WorkCard key={d.id} data={d} />
           ))}
         </div>
+
+        <span className="rotate" ref={yinyang}>
+          <YinYang width={80} height={80} fill={DarkTheme.text} />
+        </span>
       </Box>
     </ThemeProvider>
   );
