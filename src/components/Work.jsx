553c085c937247d151a810c5369668f5fb7cb872
Kawsar Ahmed Fahad <kafaha1999@gmail.com>
2022-06-30 02:01:27 +0600

    WorkDetails initialization done

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import { DarkTheme } from './Themes';
+
+import { LogoComponent } from './partials/LogoComponent';
+import { PowerButton } from './partials/PowerButton';
+import { SocialIcons } from './partials/SocialIcons';
+
+import { WorkDetails } from '../data/WorkData';
+
+const Box = styled.div`
+  background-color: ${(props) => props.theme.body};
+`;
 
 export const Work = () => {
-  return <div>Work</div>;
+  return (
+    <ThemeProvider theme={DarkTheme}>
+      <Box className="Work">
+        <LogoComponent theme="dark" />
+        <SocialIcons theme="dark" />
+        <PowerButton />
+
+        <div className="WorkDetails">
+          {WorkDetails.map((d) => (
+            <h1>WorkDetails</h1>
+          ))}
+        </div>
+      </Box>
+    </ThemeProvider>
+  );
 };
